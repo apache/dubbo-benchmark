c1e29216b4bb391cbd3259719a90c30b94e6e9e1
beiwei30 <ian.luo@gmail.com>
2018-05-29 11:08:15 +0800

    native hessian

--- client-base/src/main/java/org/apache/dubbo/Client.java
+++ client-base/src/main/java/org/apache/dubbo/Client.java
@@ -1,6 +1,5 @@
 package org.apache.dubbo;
 
-import org.apache.dubbo.benchmark.bean.User;
 import org.apache.dubbo.benchmark.service.UserService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,7 +8,9 @@ public class Client {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
         context.start();
         UserService userService = (UserService) context.getBean("userService");
-        User user = userService.getUser(1);
-        System.out.println("user: " + user);
+        System.out.println("existUser: " + userService.existUser("user@acme.com"));
+        System.out.println("getUser: " + userService.getUser(1));
+        System.out.println("createUser: " + userService.createUser(userService.getUser(2)));
+        System.out.println("listUser: " + userService.listUser(1));
     }
 }
