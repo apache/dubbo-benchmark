9448149059f2a94e005bdd785b515007805e5754
WenChen <437631891@qq.com>
2022-09-27 20:32:21 +0800

    add prepare.sh to replace dubbo.version+1

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,5 @@ DUBBO_VERSION=$1
 #get old version
 OLD_VERSION=`awk '/<dubbo.version>[^<]+<\/dubbo.version>/{gsub(/<dubbo.version>|<\/dubbo.version>/,"",$1);print $1}' ./pom.xml | uniq`
 s="s/<dubbo.version>$OLD_VERSION<\/dubbo.version>/<dubbo.version>$DUBBO_VERSION<\/dubbo.version>/g"
-sed -i s1 ./pom.xml
+sed -i s ./pom.xml
 cat ./pom.xml
